34f180745c2546bc8351a4e1c9a8e565593a82ff
sandmark <sandmark.m@gmail.com>
2019-03-05 18:42:06 +0900

    Create base app

---
+++ src/todo/main.clj
@@ -0,0 +1,12 @@
+(ns todo.main
+  (:gen-class)
+  (:require [duct.core :as duct]))
+
+(duct/load-hierarchy)
+
+(defn -main [& args]
+  (let [keys     (or (duct/parse-keys args) [:duct/daemon])
+        profiles [:duct.profile/prod]]
+    (-> (duct/resource "todo/config.edn")
+        (duct/read-config)
+        (duct/exec-config profiles keys))))
