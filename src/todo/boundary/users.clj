d712534a0e4b6387fd08246fc12695cf9a36f45d
sandmark <sandmark.m@gmail.com>
2019-03-06 18:29:04 +0900

    Implement "POST /users"
    $ curl -i -X POST -H 'Content-Type:application/json' -d \
    '{"email":"user1@example.com","password":"password123"}' localhost:3000/users

--- src/todo/boundary/users.clj
+++ src/todo/boundary/users.clj
@@ -1,9 +1,17 @@
 (ns todo.boundary.users
-  (:require [clojure.java.jdbc :as jdbc]
+  (:require [buddy.hashers :as hashers]
+            [clojure.java.jdbc :as jdbc]
             duct.database.sql))
 
-(defprotocol Users)
+(defprotocol Users
+  (create-user [db params]))
 
 
 (extend-protocol Users
-  duct.database.sql.Boundary)
+  duct.database.sql.Boundary
+  (create-user [{db :spec} {:keys [email password]}]
+    (let [password-digest (hashers/derive password)
+          result          (jdbc/insert! db :users
+                                        {:email           email
+                                         :password_digest password-digest})]
+      (-> result first :id))))
