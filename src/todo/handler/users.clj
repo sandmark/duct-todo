4d4fff077dcf88dfc7717ad9acf0dd4148e55ea2
sandmark <sandmark.m@gmail.com>
2019-03-06 19:41:25 +0900

    Add validation for email uniqueness

--- src/todo/handler/users.clj
+++ src/todo/handler/users.clj
@@ -5,15 +5,19 @@
             [struct.core :as st]
             [todo.boundary.users :as users]))
 
-(defn- create-form-schema []
-  {:email    [st/required st/email]
-   :password [st/required st/string
-              [st/min-count 8] [st/max-count 100]]})
+(defn- create-form-schema [db]
+  (let [email-uniqueness {:message  "already used"
+                          :optional true
+                          :validate (fn [v db]
+                                      (nil? (users/find-user-by-email db v)))}]
+    {:email    [st/required st/email [email-uniqueness db]]
+     :password [st/required st/string
+                [st/min-count 8] [st/max-count 100]]}))
 
 
 (defmethod ig/init-key ::create [_ {:keys [db]}]
   (fn [{:keys [body-params]}]
-    (if-let [errors (->> (create-form-schema) (st/validate body-params) first)]
+    (if-let [errors (->> (create-form-schema db) (st/validate body-params) first)]
       [::response/bad-request errors]
       (let [id (users/create-user db body-params)]
         [::response/created (str "/users/" id)]))))
