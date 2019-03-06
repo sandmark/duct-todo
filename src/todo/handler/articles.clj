e3d04819be5df320224a699ec7a98b67b2ca7be4
sandmark <sandmark.m@gmail.com>
2019-03-06 16:21:50 +0900

    Fix example handler to work

--- src/todo/handler/articles.clj
+++ src/todo/handler/articles.clj
@@ -1,7 +1,10 @@
 (ns todo.handler.articles
   (:require [ataraxy.response :as response]
+            [clojure.java.jdbc :as jdbc]
             [integrant.core :as ig]))
 
 (defmethod ig/init-key ::index [_ {:keys [db]}]
   (fn [request]
-    [::response/ok "OK!"]))
+    (let [articles (jdbc/query (:spec db)
+                               ["SELECT * FROM articles"])]
+      [::response/ok articles])))
